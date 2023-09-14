d804e723d48bda16320c0ab09d8ccd2c70c92e01
lucca <luccabrasi.es@gmail.com>
2023-09-14 08:25:28 -0300

    add animacoes

--- botaoChat.js
+++ botaoChat.js
@@ -1,36 +1,50 @@
 const botao = document.querySelector('.buttonSection');
 const chatbot = document.querySelector('.chatBot');
 const exit = document.querySelector('.conteudo-x');
+const iframe = document.querySelector('#iframe')
 
 let contador = true
 
 botao.addEventListener('click', () => {
-        
-        if(contador === true){
-            botao.classList.add('expande');
-            exit.style.display = 'flex';
-            chatbot.classList.add('chatBotExpandido');
 
-            contador = false
-        }
+    if (contador === true) {
         
+        iframe.classList.add('expandeChat')
+        botao.classList.add('expande');
+        chatbot.classList.add('chatBotExpandido');
+        
+
+        contador = false
+
+        setTimeout(()=>{
+            iframe.classList.remove('expandeChat')
+            exit.style.display = 'flex';
+        },1000)
+    }
+
 });
 
 exit.addEventListener('click', () => {
-        
-    if(contador === false){
-        chatbot.classList.remove('chatBotExpandido');
-        chatbot.classList.add('chatBotContraido');
+
+    if (contador === false) {
+        iframe.classList.add('contraiChat')
+
         exit.style.display = 'none';
-    
+
         botao.classList.add('contrai');
         botao.classList.remove('expande');
 
-        setTimeout(()=>{
+        setTimeout(() => {
+            chatbot.classList.remove('chatBotExpandido');
+            chatbot.classList.add('chatBotContraido');
+        }, 800)
+
+        setTimeout(() => {
             botao.classList.remove('contrai')
             chatbot.classList.remove('chatBotContraido');
+            iframe.classList.remove('contraiChat')
             contador = true
-        },1200)
+        }, 1200)
 
     }
 });
